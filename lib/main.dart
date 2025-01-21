c6a8af68ab4e7d77443d2927fc2106dd7aaf9933
Febin Johnson <febinnj1996@gmail.com>
2025-01-21 22:32:04 +0530

    added routes to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:zartek/core/routes/app_routes.dart';
 import 'package:zartek/firebase_options.dart';
 import 'package:zartek/injector.dart';
 
@@ -15,12 +16,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'zartek',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      routeInformationProvider: AppRoutes.appRoutes.routeInformationProvider,
+      routeInformationParser: AppRoutes.appRoutes.routeInformationParser,
+      routerDelegate: AppRoutes.appRoutes.routerDelegate,
     );
   }
 }
